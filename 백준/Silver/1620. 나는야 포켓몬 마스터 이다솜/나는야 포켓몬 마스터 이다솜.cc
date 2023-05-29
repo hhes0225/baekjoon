dfaf1ce731524bd7003404ea5b5ba7dc238aa308
HHS <43839695+hhes0225@users.noreply.github.com>
2023-05-29 18:20:45 +0200

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 164 ms, Memory: 20256 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,37 +1,53 @@
-#include <iostream>
+#include<iostream>
 #include <string>
 #include <vector>
 #include <map>
 using namespace std;
-//해시맵사용
-int main(){
-    map<string, int> pokemon;
-    vector<string> name;
-    int n, m;
-    string temp; //포켓몬 이름과 번호가 섞여있으므로 string
-    cin >> n >> m;
-    vector<string> result; 
-    for (int i = 1; i <= n;i++){
-        cin >> temp; 
-        name.push_back(temp); // 도감번호 주어질때 포켓몬 이름 찾기
-        pokemon.insert(make_pair(temp, i)); // 포켓몬 이름이 주어질때 도감번호 빠르게 찾기
-    }
-    for (int i = 0; i < m; i++){
-        cin >> temp;       
-        if(temp[0] >= 65 && temp[0] <= 90){ 
-        // 포켓몬 이름이 주어진 경우 (맨 앞 문자가 영어 대문자인것을 이용)
-            result.push_back(to_string(pokemon[temp]));
-            //result 벡터는 string이기 때문에 번호를 string으로 변환해줌
-        }
-        else // 도감 번호가 주어진 경우
-        {
-            result.push_back(name[stoi(temp)-1]);
-            //입력값은 string 그래서 정수형으로 변환 후 넣어줌
-            // -1을 해준 이유는 name배열은 0부터 시작했기 때문 
-        }
-    }
-    for (int i = 0; i < result.size();i++){
-        cout << result[i] << '\n'; // 출력
-    }
-    return 0;
+
+int main(void) {
+	/*
+		맵=딕셔너리에서 찾는 법
+		1. key로 value 찾기 = map의 메서드인 find(key) 사용
+		2. value로 key 찾기 = 순회하면서 사용
+		이때, 숫자 범위 내라면(>='0' && <='9') 숫자 변환 함수인 stoi 사용
+		stoi <---> to_string
+	*/
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int dictNum, questionNum;
+	string tmpStr;
+	vector<string> namelist;
+	map<string, int> pokemonDict;
+	vector<string> qna;
+
+	cin >> dictNum >> questionNum;
+
+
+	for (int i = 0; i < dictNum; i++) {
+		cin >> tmpStr;
+
+		pokemonDict.insert(make_pair(tmpStr, i+1));
+		namelist.push_back(tmpStr);
+	}
+
+	for (int i = 0; i < questionNum; i++) {
+		cin >> tmpStr;
+		if (tmpStr[0] >= 'A'&& tmpStr[0]<='Z') {
+			cout << pokemonDict[tmpStr] << "\n";
+			//qna.push_back(to_string(pokemonDict[tmpStr]));
+		}
+		else {
+			
+			cout << namelist[stoi(tmpStr) - 1] << "\n";
+			//qna.push_back(namelist[stoi(tmpStr)-1]);
+		}
+	}
+
+	/*for (int i = 0; i < qna.size(); i++) {
+		cout << qna[i] << '\n';
+	}*/
+
+
+	return 0;
 }
