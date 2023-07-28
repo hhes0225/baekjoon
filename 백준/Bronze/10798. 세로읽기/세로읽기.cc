2e60fae6549bb8f1ef89a8c89077de175c0208a9
HHS <43839695+hhes0225@users.noreply.github.com>
2023-07-29 03:13:15 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+	string input;
+	vector<string> words;
+	int longestWord=0;
+
+	for (int i = 0; i < 5; i++) {
+		cin >> input;
+		words.push_back(input);
+	}
+
+
+	//가장 긴 문자열 길이
+	for (int i = 0; i < words.size(); i++) {
+		longestWord = max((int)words[i].size(), longestWord);
+	}
+
+	//cout << longestWord << endl;
+
+	for (int i = 0; i < longestWord+1; i++) {
+		for (int j = 0; j < words.size(); j++) {
+			if (words[j].size() < i || words[j][i]==NULL) {
+				continue;
+			}
+			cout << words[j][i];
+		}
+	}
+
+	cout << '\n';
+
+	return 0;
+}
