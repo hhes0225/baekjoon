283f2db9bac50b715a28c4006c6af298f857ffe0
HHS <43839695+hhes0225@users.noreply.github.com>
2023-12-09 03:40:37 +0100

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+
+    int input;
+
+    int original;
+
+    
+
+    cin>>input;
+
+    
+
+    original=input*10/11;
+
+    
+
+    cout<<original<<endl;
+
+    
+
+    return 0;
+
+}
