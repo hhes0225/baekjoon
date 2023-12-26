4e9c31156136fb96c53ab0d0dc9757bb749db816
HHS <43839695+hhes0225@users.noreply.github.com>
2023-12-26 21:52:10 +0100

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int year;
+    int startYear=1946;
+    
+    cin>>year;
+    
+    cout<<year-startYear<<endl;
+    
+    return 0;
+}
