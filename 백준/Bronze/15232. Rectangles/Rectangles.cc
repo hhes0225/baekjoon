8c32b80d0524fa9f3b1cedfadf4c4d2cd59195d4
HHS <43839695+hhes0225@users.noreply.github.com>
2024-02-05 10:17:31 +0100

    [Bronze IV] Title: Rectangles, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int width, height;
+
+    cin>>height>>width;
+
+    for(int i=0;i<height;i++){
+        for(int j=0;j<width;j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    
+    return 0;
+}
