02da04cff930471c3fe55725f86d2073da02f227
HHS <43839695+hhes0225@users.noreply.github.com>
2024-05-18 00:08:31 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㅋ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23810. 골뱅이 찍기 － 뒤집힌 ㅋ/골뱅이 찍기 － 뒤집힌 ㅋ.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+int main() {
+    int tCase;
+    cin>>tCase;
+
+    for(int i=0;i<tCase*5;i++){
+
+        if(i<tCase){
+            for(int j=0;j<tCase*5;j++){
+                cout<<"@";
+            }
+        }
+        else if(i>=tCase*2 && i<tCase*2+tCase){
+            for(int j=0;j<tCase*5;j++){
+                cout<<"@";
+            }
+        }
+        else{
+            for(int j=0;j<tCase;j++){
+                cout<<"@";
+            }
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
