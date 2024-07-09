b52f4bced196e6bbd8b085c368eba87c52ae3cdb
HHS <43839695+hhes0225@users.noreply.github.com>
2024-07-10 05:19:31 +0900

    [Bronze III] Title: Not A + B, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23530. Not A ＋ B/Not A ＋ B.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+    int tCase;
+    cin>>tCase;
+
+    for(int i=0;i<tCase;i++){
+        int a, b;
+        cin>>a>>b;
+
+        cout<<1<<endl;
+    }
+    
+    return 0;
+}
