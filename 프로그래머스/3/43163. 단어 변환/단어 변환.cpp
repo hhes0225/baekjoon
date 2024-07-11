f2a2ee98c9999ca581e4d2b1c199c8ec1ba399b4
HHS <43839695+hhes0225@users.noreply.github.com>
2024-07-11 22:33:18 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 4.01 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int answer = 100;
 
-
+//isChangeable로 아예 1인지 아닌지 if 체크도 여기서 하는게 좋을듯
 int checkDiff(string begin, string target){
     int diff = 0;
     
@@ -22,23 +22,30 @@ int checkDiff(string begin, string target){
     return diff;
 }
 
-void dfs(string begin, string target, vector<string> words, vector<bool> visited, int step){    
+//words, visited & 넣기
+void dfs(string begin, string target, vector<string> words, vector<bool> visited, int step){ 
+    //현재 step이 answer보다 큰 경우 더 탐색할 필요가 없음(answer보다 작아지는 경우를 탐색하는게 목표이므로)
+    //미리 탐색을 중단하는 것이 좋음(최적화)
+
     //문자열이 완벽하게 같은 경우, 결과의 최소값 체크.
     if(begin==target){
         answer=min(answer, step);
     }
     
-    //문자열의 차이가 1개인 경우, 자식 노드 순회
+    //배열 순회, 문자열의 차이가 1개인 경우만 자식 노드로 두기 위함
     for(int i=0;i<words.size();i++){
         //문자열의 차이가 1개가 아닌 경우, 체크 X
         if(checkDiff(begin, words[i])!=1){
             continue;
         }
         else if(visited[i]==true){
+            //전역X 매개변수로 둔 이유 : 각 step별로 방문한 노드는 true처리/다른 step에는 이것의 영향을 받지 않고 독립적으로 시행하기 위함
+            //ex) hot->dot->dog->log->cog : 전역으로 한다면 다음 예시 처리되지 않음
+            //ex) hot->dot->dog->cog
             continue;
         }
         else{
-            cout<<words[i]<<endl;
+            //cout<<words[i]<<endl;
             visited[i]=true;
             dfs(words[i], target, words, visited, step+1);
         }
