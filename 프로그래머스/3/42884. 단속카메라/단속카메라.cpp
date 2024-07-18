aff58f8ac899897f116ad2c350041d3546c73700
HHS <43839695+hhes0225@users.noreply.github.com>
2024-07-18 22:49:44 +0900

    [level 3] Title: 단속카메라, Time: 3.78 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -4,7 +4,13 @@
 #include <algorithm>
 using namespace std;
 
-// 진출 지점을 기준으로 
+//그리디 단골유형
+//UCPC 기준으로 진출 기준 정렬 & 탐색
+
+// 진출 지점을 기준으로 카메라 설치
+// (init. 진출지점 기준 오름차순 정렬 후 첫 번째 루트의 진출지점)
+// 순회 루트의 진입 지점이 기존 설치 진출지점보다 크다면 루트가 겹치지 않으므로 카메라 설치
+// (순회 당시 진입과 세트인 진출지점에, 카메라 설치 포인트 갱신)
 
 bool cmp(vector<int> a, vector<int> b){
     return a[1]<b[1];
@@ -36,3 +42,6 @@ int solution(vector<vector<int>> routes) {
     
     return answer;
 }
+
+//참고
+//https://school.programmers.co.kr/questions/72005
