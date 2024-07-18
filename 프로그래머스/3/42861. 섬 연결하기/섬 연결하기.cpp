52bac13cb6e41a5eac6502c4311ff4fa8834fa62
HHS <43839695+hhes0225@users.noreply.github.com>
2024-07-18 22:28:02 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 3.76 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+//MST인 이유 : 최소의 비용으로 모든 섬이 서로 통행 가능하도록 만들 때(MST의 정의임)
+
 // 무방향 weighted 그래프
 // 최소 비용 -> 최소 신장트리(Minimum Spanning Tree) 로 해결
 // 크루스칼 or 프림 알고리즘 적용
@@ -40,6 +42,7 @@ int solution(int n, vector<vector<int>> costs) {
     makeGraph(n, costs);
     
     //첫 번째 노드 방문 처리
+    //**프림 알고리즘은 시작점을 지정한다
     visited.push_back(0);
     
     //나머지 노드 미방문 처리
@@ -47,15 +50,23 @@ int solution(int n, vector<vector<int>> costs) {
         unvisited.push_back(i);
     }
     
+    //시작 정점 제외한 n-1개 정점 선택
     for(int i=1;i<n;i++){
         int min = INT_MAX;
         int minIdx = 0;
         
+        //visited 순회
         for(int j=0;j<i;j++){
+            //unvisited 순회
             for(int k=0;k<n-i;k++){
+                
+                //visited와 unvisited 사이 인접 정점 중 가중치 가장 작은 정점 선택
+                //인접해 있는 정점 -> (graph[visited[j]][unvisited[k]] > 0
+                
                 if(weightedGraph[visited[j]][unvisited[k]]>0&&min>weightedGraph[visited[j]][unvisited[k]]){
                     min=weightedGraph[visited[j]][unvisited[k]];
                     minIdx=k;
+                    //최종적으로 결정된 min을 가진 unvisited 정점을 삭제하고 visited에 추가해야 하므로 min 업뎃할 때 min_index에 해당 unvisited 정점을 보관해두어 같이 업데이트
                 }
             }
         }
@@ -63,6 +74,10 @@ int solution(int n, vector<vector<int>> costs) {
         visited.push_back(unvisited[minIdx]);
         unvisited.erase(unvisited.begin()+minIdx);
         answer+=min;
+        //결정되면
+        //visited에 unvisited[min_index]을 추가한다.
+        //unvisited[min_index]는 삭제한다.
+        //비용 합산
     }
     
     return answer;
