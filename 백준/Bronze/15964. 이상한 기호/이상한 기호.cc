30f69e52643a2e7ca1a532b26da77b99f3a680cc
HHS <43839695+hhes0225@users.noreply.github.com>
2024-10-14 04:58:11 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    long long res;
+    int a, b;
+    cin>>a>>b;
+
+    cout<<(a+b)*(a-b)<<"\n";
+    
+
+    
+    
+    return 0;
+}
