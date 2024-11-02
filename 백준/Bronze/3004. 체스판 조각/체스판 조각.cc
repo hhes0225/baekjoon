34fc840f164dde95361b7a06cab23734590cadd2
HHS <43839695+hhes0225@users.noreply.github.com>
2024-11-03 02:45:57 +0900

    [Bronze III] Title: 체스판 조각, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/3004. 체스판 조각/체스판 조각.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int N, res;
+    scanf("%d", &N);
+    if (N % 2 == 0)
+    {
+        res = (N/2 + 1) * (N/2 + 1);
+    }
+    else
+        res = ((N+1)/2) * ((N+3)/2); 
+    printf("%d", res);
+}
