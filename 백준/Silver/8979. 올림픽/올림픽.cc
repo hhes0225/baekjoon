c23300962cb0dc39ec7df9160a6888b36b62bb88
HHS <43839695+hhes0225@users.noreply.github.com>
2024-11-06 18:14:02 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2028 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.cc
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -32,22 +32,19 @@ int main() {
 
     sort(scores.begin(), scores.end(), compare);
 
-    if(country==scores[0][0]){
-        cout<<rank<<"\n";
-        return 0;
-    }
-    
-    for(int i=1;i<len;i++){
-        if(scores[i-1][1]!=scores[i][1]||scores[i-1][2]!=scores[i][2]||scores[i-1][3]!=scores[i][3]){
-            rank++;
+    for (int i = 0; i < len; i++) {
+        // 이전 국가와 메달 수가 다를 때에만 rank를 업데이트
+        if (i > 0 && (scores[i][1] != scores[i - 1][1] || scores[i][2] != scores[i - 1][2] || scores[i][3] != scores[i - 1][3])) {
+            rank = i + 1;
         }
 
-        if(country==scores[i][0]){
-            cout<<rank;
+        // 찾고자 하는 국가의 등수를 출력하고 종료
+        if (scores[i][0] == country) {
+            cout << rank << "\n";
+            return 0;
         }
-
     }
-    cout<<"\n";
+
     
     return 0;
 }
