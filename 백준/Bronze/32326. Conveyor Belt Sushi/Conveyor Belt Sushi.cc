e50f7c6b465560c735fd9342c98619ff9a7ae213
HHS <43839695+hhes0225@users.noreply.github.com>
2024-11-18 02:36:12 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int red, green, blue;
+    cin>>red>>green>>blue;
+
+    int totalPrice=red*3+green*4+blue*5;
+
+    cout<<totalPrice<<"\n";
+    return 0;
+}
