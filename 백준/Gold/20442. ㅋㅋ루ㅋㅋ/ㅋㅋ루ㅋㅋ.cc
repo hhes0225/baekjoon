8bbcfb1c6e5099931ddbd2e00186615777873612
HHS <43839695+hhes0225@users.noreply.github.com>
2024-11-21 22:34:01 +0900

    Update ㅋㅋ루ㅋㅋ.cc

--- 백준/Gold/20442. ㅋㅋ루ㅋㅋ/ㅋㅋ루ㅋㅋ.cc
+++ 백준/Gold/20442. ㅋㅋ루ㅋㅋ/ㅋㅋ루ㅋㅋ.cc
@@ -17,55 +17,55 @@ using namespace std;
 string input;
 
 //해당 인덱스 기준 왼쪽에 있는 k 개수와, 오른쪽에 있는 k 개수 저장 
-int left_k_cnt[3000005];
-int right_k_cnt[3000005];
+int leftKCount[3000005];
+int rightKCount[3000005];
 
 //r이 있는 위치 저장
-vector<int> r_indexs;
+vector<int> RIndice;
 
 int main()
 {
 	cin >> input;
 
 	//인덱스 i 왼쪽에 몇 개의 k 가 존재하는가?
-	int k_cnt = 0;
+	int kCount = 0;
 	for (int i = 0; i < input.size(); i++) {
 		if (input[i] == 'K') {
-			k_cnt++;
+			kCount++;
 			continue;
 		}
 
-		r_indexs.push_back(i);
-		left_k_cnt[i] = k_cnt;
+		RIndice.push_back(i);
+		leftKCount[i] = kCount;
 	}
 
 	//인덱스 i 오른쪽에 몇 개의 k가 존재하는가?
-	k_cnt = 0;
+	kCount = 0;
 	for (int i = input.size() - 1; i >= 0; i--) {
 		if (input[i] == 'K') {
-			k_cnt++;
+			kCount++;
 			continue;
 		}
 
-		right_k_cnt[i] = k_cnt;
+		rightKCount[i] = kCount;
 	}
 
 	//중간에서 만나는 투 포인터를 이용
 	int left = 0;
-	int right = r_indexs.size() - 1;
+	int right = RIndice.size() - 1;
 	int result = 0;
 	while (left <= right) {
 
 		//left기준 왼쪽의 k 개수와, right기준 오른쪽의 k 개수 확인
-		int k_cnt1 = left_k_cnt[r_indexs[left]];
-		int k_cnt2 = right_k_cnt[r_indexs[right]];
+		int kCount1 = leftKCount[RIndice[left]];
+		int kCount2 = rightKCount[RIndice[right]];
 
 		//right - left + 1 : 가운데 있는 r의 개수
-		//min(k_cnt1, k_cnt2) * 2: 현재 양 끝에서 사용할 수 있는 최소 K
-		result = max(result, right - left + 1 + (min(k_cnt1, k_cnt2) * 2));
+		//min(kCount1, kCount2) * 2: 현재 양 끝에서 사용할 수 있는 최소 K
+		result = max(result, right - left + 1 + (min(kCount1, kCount2) * 2));
 
 		//왼쪽의 k가 더 많을때
-		if (k_cnt1 > k_cnt2) {
+		if (kCount1 > kCount2) {
 			right--;
 		}
 
