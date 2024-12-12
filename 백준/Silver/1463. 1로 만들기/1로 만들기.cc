ad843f9cb6bdff7ecb72ac731f234d6af5c3881b
HHS <43839695+hhes0225@users.noreply.github.com>
2024-12-13 04:21:58 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,46 +1,43 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 vector<int> dp;
 
+int dpFunc(int n){
+    dp[0]=0;
+    dp[1]=0;
 
+    for(int i=2;i<=n;i++){
+        int div3=987'654'321;
+        int div2=987'654'321;
+        int sub1=dp[i-1];
 
-int tabulation(int n){
-    if(n==1){
-        return 0;
-    }
-    else if(n==2 || n==3){
-        return 1;
-    }
-    
-    dp[1]=0, dp[2]=1, dp[3]=1;
-
-    for(int i=4;i<=n; i++){
-        //모든 수는 기본적으로 case 3에 해당: 1을 빼기
-        dp[i]=dp[i-1]+1;
-        
-        //여기서 3으로 나누어 떨어진다면, 그리고 나누었을 때 케이스가 더 작으면 값 변경
         if(i%3==0){
-            dp[i]=min(dp[i], dp[i/3]+1);
+            div3=dp[i/3];
         }
-        //여기서 2으로 나누어 떨어진다면, 그리고 나누었을 때 케이스가 더 작으면 값 변경
         if(i%2==0){
-            dp[i]=min(dp[i], dp[i/2]+1);
+            div2=dp[i/2];
         }
-        
+
+        dp[i]=min({div3, div2, sub1})+1;
     }
+    /*
+    for(auto it:dp){
+        cout<<it<<" ";
+    }
+    cout<<"\n";
+    */
     
     return dp[n];
 }
 
-int main(void){
-    dp.resize(1000000+1);
-    int input;
-    cin>>input;
+int main() {
+    int n;
+    cin>>n;
 
-    cout<<tabulation(input)<<"\n";
+    dp.assign(n+1, 0);
+
+    cout<<dpFunc(n)<<"\n";
     
-
     return 0;
 }
