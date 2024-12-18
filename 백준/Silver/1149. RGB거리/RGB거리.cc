e62ecdcbd2329fde5b1405bf1b90c328315c8638
HHS <43839695+hhes0225@users.noreply.github.com>
2024-12-18 15:34:40 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,104 +1,60 @@
 #include <bits/stdc++.h>
-#define FOR(i, n) for(int i=0;i<n;i++)
-
-#define R 0
-#define G 1
-#define B 2
-
 using namespace std;
 
-int n;
-vector<vector<int>> colorCost;
 vector<vector<int>> dp;
-//dp[i][color]: i번째 집을 color로 칠할 때 최소 비용
-
-int dpFunc(){
-    dp[0][R]=colorCost[0][R];
-    dp[0][G]=colorCost[0][G];
-    dp[0][B]=colorCost[0][B];
-
-    for(int i=1;i<n;i++){
-        dp[i][R]=colorCost[i][R]+min(dp[i-1][G], dp[i-1][B]);
-        dp[i][G]=colorCost[i][G]+min(dp[i-1][R], dp[i-1][B]);
-        dp[i][B]=colorCost[i][B]+min(dp[i-1][G], dp[i-1][R]);
-    }
-
-    return min({dp[n-1][R], dp[n-1][G], dp[n-1][B]});
-}
-
-
 /*
-잘못된 방식.. 1차원 dp
-int dpFunc(int initColor){
-    fill(dp.begin(), dp.end(), 0);
-    
-    //이전 집  색 저장
-    int prevColor=initColor;
-    //첫 번째 집 칠하기
-    dp[0]=colorCost[0][initColor];
+    dp[i][j]: i번 집이 j 색을 칠했을 경우 비용의 최소값
+    따라서 다음 집을 보지 않고 이전 집을 본다.
+    이전 집에서 내가 선택한 집의 색이 아닌 다른 색을 칠한 경우들을 보고, 최소값 채택
 
-    
-    for(int i=1;i<n;i++){
-        if(prevColor==R){
-            if(colorCost[i][G]<colorCost[i][B]){
-                prevColor=G;
-            }
-            else{
-                prevColor=B;
-            }
-            
-            dp[i]=dp[i-1]+min(colorCost[i][G], colorCost[i][B]);
-        }
-        else if(prevColor==G){
-            if(colorCost[i][R]<colorCost[i][B]){
-                prevColor=R;
-            }
-            else{
-                prevColor=B;
-            }
-            
-            dp[i]=dp[i-1]+min(colorCost[i][R], colorCost[i][B]);
-        }
-        else{//initColor==B
-            if(colorCost[i][G]<colorCost[i][R]){
-                prevColor=G;
-            }
-            else{
-                prevColor=R;
-            }
-            
-            dp[i]=dp[i-1]+min(colorCost[i][G], colorCost[i][R]);
-        }
-    }
-    
-
-    for(int it:dp){
-        cout<<it<<" ";
-    }
-    cout<<"\n";
-
-    return dp[n-1];
-}
-
+    설정한 점화식:
+    dp[i][내가 선택한 색]=min(dp[i-1][안선택한 색1], dp[i-1][안선택한 색2])+house[i][선택한 색];
 */
 
-int main() {
+int dpFunc(const vector<vector<int>> &house, int n){
+    //init
+    for(int i=0;i<3;i++) dp[1][i]=house[1][i];
 
-    cin>>n;
+    for(int i=2;i<=n;i++){
+        //r 선택한 경우
+        dp[i][0]=house[i][0]+min(dp[i-1][1], dp[i-1][2]);
+        
+        //g 선택한 경우
+        dp[i][1]=house[i][1]+min(dp[i-1][0], dp[i-1][2]);
 
-    colorCost.resize(n, vector<int>(3));
-    dp.resize(n, vector<int>(3));
-
-    for(int i=0;i<n;i++){
-        for(int j=0;j<3;j++){
-            cin>>colorCost[i][j];
-        }
+        //b 선택한 경우
+        dp[i][2]=house[i][2]+min(dp[i-1][0], dp[i-1][1]);
     }
 
-    int result=dpFunc();
+    /*
+    //출력
+    for(auto i:dp){
+        for(auto j:i){
+            cout<<j<<" ";
+        }
+        cout<<"\n";
+    }
+    */
 
-    cout<<result<<"\n";
+    return *min_element(dp[n].begin(), dp[n].end());
+}
+
+int main() {
+    int n;
+    vector<vector<int>> house;
     
+    cin>>n;
+
+    dp.assign(n+1, vector<int>(3, 0));
+    house.assign(n+1, vector<int>(3,0));
+
+    for(int i=1;i<=n;i++){
+        cin>>house[i][0];//r
+        cin>>house[i][1];//g
+        cin>>house[i][2];//b
+    }
+
+    cout<<dpFunc(house, n)<<"\n";
     
     return 0;
 }
