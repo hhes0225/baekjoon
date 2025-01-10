90e5de72a5942d4579efc8b3f71495e46b31609a
HHS <43839695+hhes0225@users.noreply.github.com>
2025-01-10 16:31:20 +0900

    [Gold II] Title: 숫자구슬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2613. 숫자구슬/숫자구슬.cc
+++ 백준/Gold/2613. 숫자구슬/숫자구슬.cc
@@ -1,38 +1,101 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
- 
-int n, m, i, j, a[301] = { 0 }, b[300];
- 
-bool go(int lim) {
-    int cnt = m;
-    i = 0;
-    while (i < n) {
-        j = n - cnt + 1;
-        while (i<j && a[j] - a[i]>lim)--j;
-        if (i == j)return false;
-        b[--cnt] = j - i;
-        i = j;        
+
+int n, m;
+vector<int> beads;
+vector<int> groups;
+
+bool isDividable(int maxVal){
+    int sum=0;
+    int cnt=1;
+    
+    for(int i=0;i<beads.size();i++){
+        sum+=beads[i];//비즈 누적해서서
+
+        //누적값이 최댓값을 초과하게 되면
+        if(sum>maxVal){
+            //현재 인덱스부터 새 그룹으로 넘어가기
+            //이전 인덱스까지 컷
+            cnt++;
+            sum=beads[i];
+        }
+        //그렇지 않으면 다음 요소 더해가면서 최댓값 초과하도록 만듦
     }
-    return cnt == 0;
+
+    return cnt<=m;
 }
- 
+
+void makeGroup(int maxVal){
+    int sum=0;
+    int elements=0;
+    int groupNum=m;
+
+    for(int i=0;i<beads.size();i++){
+        sum+=beads[i];
+
+        if(sum>maxVal){
+            groups.push_back(elements);
+            groupNum--;
+            elements=0;
+            sum=beads[i];
+        }
+        
+        elements++;
+
+        //1 그룹당 1개는 있어야 하므로
+        //남은 구슬 개수가 그룹 수와 동일하면 그만큼 넣어줌
+        //예를 들어, gNum=3이면 {..4 1 1} 이런식으로
+        //먼저 남은 누적 수 넣고, 뒤에 gNum만큼 1 추가해주기
+        if(n-i==groupNum){
+            break;
+        }
+    }
+
+    for(int i=0;i<groupNum;i++){
+        groups.push_back(elements);
+        elements=1;
+    }
+}
+
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(0);
- 
-    cin >> n >> m;
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i + 1];
-        a[i + 1] += a[i];
+    cin>>n>>m;
+
+    beads.resize(n);
+
+    int sum=0;
+    for(int i=0;i<n;i++){
+        cin>>beads[i];
+        sum+=beads[i];
     }
- 
-    int l = 1, r = a[n];
-    while (l <= r) {
-        int mid = (l + r) / 2;
-        if (go(mid))r = mid - 1;
-        else l = mid + 1;
+
+    int left=*max_element(beads.begin(), beads.end());
+    int right=sum;
+
+    //최댓값의 최소화!!
+    //이분탐색에서는 최소화만 해주면 됨.
+    //그룹의 최댓값 구하는 로직은 함수에서 체크하면 됨
+    while(left<=right){
+        int mid=(left+right)/2;
+
+        if(isDividable(mid)){
+            right=mid-1;
+            //성공했으면 이것보다 작은 것에 대해 확인
+            //최소화를 해야 하므로!!
+        }
+        else{
+            left=mid+1;
+            //실패하면 어쩔 수 없이 큰 값으로 고고링
+        }
     }
- 
-    cout << l << endl;
-    go(l);
-    for (int i = m - 1; i >= 0; --i)cout << b[i] << ' ';
+
+    cout<<left<<"\n";
+    makeGroup(left);
+
+    for(auto i:groups){
+        cout<<i<<" ";
+    }
+    cout<<"\n";
+    
+    
+    return 0;
 }
