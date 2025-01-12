76b4d770988adebce9d1458bd77e1e1bbf14f094
HHS <43839695+hhes0225@users.noreply.github.com>
2025-01-12 18:18:16 +0900

    [Silver II] Title: 점프 점프, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11060. 점프 점프/점프 점프.cc
+++ 백준/Silver/11060. 점프 점프/점프 점프.cc
@@ -1,98 +1,54 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-
+#include <bits/stdc++.h>
+#define MAX 987'654'321
 using namespace std;
 
-/*
-	boj11060
+vector<int> board;
+vector<int> dp;
 
-	재환이가 1×N 크기의 미로에 갇혀있다. 미로는 1×1 크기의 칸으로 이루어져 있고, 각 칸에는 정수가 하나 쓰여 있다. 
-	i번째 칸에 쓰여 있는 수를 Ai라고 했을 때, 재환이는 Ai이하만큼 오른쪽으로 떨어진 칸으로 한 번에 점프할 수 있다. 
-	예를 들어, 3번째 칸에 쓰여 있는 수가 3이면, 재환이는 4, 5, 6번 칸 중 하나로 점프할 수 있다.
+int jumpBoard(int n){
+    dp[0]=0;
+    //if(board[0]!=0)
+    //    dp[board[0]]=1;
 
-	재환이는 지금 미로의 가장 왼쪽 끝에 있고, 가장 오른쪽 끝으로 가려고 한다. 
-	이때, 최소 몇 번 점프를 해야 갈 수 있는지 구하는 프로그램을 작성하시오. 
-	만약, 가장 오른쪽 끝으로 갈 수 없는 경우에는 -1을 출력한다.
-*/
+    for(int i=0;i<n-1;i++){//점프 칸 순회
+        //cout<<i<<" "<<board[i]<<"\n";
+        
+        for(int j=1;j<=board[i];j++){
+            if(i+j>=n){
+                break;
+            }
+            else{
+                dp[i+j]=min(dp[i+j], dp[i]+1);
+            }
+        }
+    }
 
-vector<vector<int>> adjList;
-vector<int> visited;
+    /*
+    for(auto it:dp){
+        cout<<it<<" ";
+    }
+    cout<<"\n";
+    */
 
-int vertices;
-
-int jump = 0;
-
-void bfs() {
-	//첫 노드 방문 처리
-	visited[0] = jump++;
-
-	//큐 생성, enqueue
-	queue<int> qsearch;
-	qsearch.push(0);
-
-	while (!qsearch.empty()) {
-		//dequeue
-		int here = qsearch.front();
-		qsearch.pop();
-
-		for (int i = 0; i < adjList[here].size(); i++) {
-			int next = adjList[here][i];
-			
-			//범위 이내라면
-			if (next < vertices) {
-				//미방문 상태라면
-				if(visited[next] == -1){
-					//방문처리 / enqueue / 점프횟수 + 1
-					visited[next] = visited[here] + 1;
-					qsearch.push(next);
-				}
-			}
-		}
-	}
-
+    if(dp[n-1]==MAX){
+        dp[n-1]=-1;
+    }
+    
+    return dp[n-1];
 }
 
+int main() {
+    int n;
+    cin>>n;
 
-int main(void) {
-	cin >> vertices;
+    board.resize(n);
+    dp.assign(n, MAX);
 
-	int step;
+    for(int i=0;i<n;i++){
+        cin>>board[i];
+    }
 
-	adjList.resize(vertices);
-	visited = vector<int>(vertices, -1);
-
-	for (int i = 0; i < vertices; i++) {
-		cin >> step;
-
-		for (int j = 0; j < step; j++) {
-			adjList[i].push_back(i + j+1);
-		}
-
-		sort(adjList[i].begin(), adjList[i].end());
-	}
-
-	/*for (int i = 0; i < vertices; i++) {
-
-		for (int j = 0; j < adjList[i].size(); j++) {
-			cout << adjList[i][j] << " ";
-
-		}
-
-		cout << endl;
-	}*/
-
-	bfs();
-
-	/*for (int i = 0; i < visited.size(); i++) {
-		cout << visited[i] << endl;
-	}*/
-
-	cout << visited[visited.size() - 1] << '\n';
-	// 갈 수 없는 경우 체크하지 않아도 됨 - 어차피 -1로 초기화했기 때문에 
-	// 분기 체크하고 갈 수 없으면 어차피 값이 -1임. 그래서 -1 출력을 굳이 안해도 됨
-
-
-	return 0;
+    cout<<jumpBoard(n)<<"\n";
+    
+    return 0;
 }
