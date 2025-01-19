1a696dac73a4ba8512adbb3727b631379ca46c6d
HHS <43839695+hhes0225@users.noreply.github.com>
2025-01-20 05:50:33 +0900

    [Bronze V] Title: 徒競走 (Footrace), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33165. 徒競走 （Footrace）/徒競走 （Footrace）.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T, V;
+    cin >> T >> V; // 시간(T)과 속도(V) 입력
+
+    int distance = T * V; // 거리 계산
+
+    cout << distance << endl; // 결과 출력
+
+    return 0;
+}
