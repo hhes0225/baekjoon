6e8c170c1768c9c1e5e5b37de69051e95722d387
HHS <43839695+hhes0225@users.noreply.github.com>
2025-03-07 17:26:40 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250307/Date to Date/date-to-date.cpp
+++ 250307/Date to Date/date-to-date.cpp
@@ -11,19 +11,24 @@ int main() {
 
     int ans=0;
 
-    //시작 달: 한 달의 해당 일자부터 남은 일수 더하기
-    ans+=(numOfDays[m1]-d1+1);//+1한 이유는 자기 자신 포함해야 하기 때문
-    //cout<<numOfDays[m1]-d1<<"\n";
-
-    //끼이는 중간 달: 그냥 통으로 더해주기
-    for(int i=m1+1;i<m2;i++){
-        ans+=numOfDays[i];
-    }
 
     if(m1!=m2)
-        //마지막달: d2 일수 더하기
-        ans+=d2;
+    {//시작 달: 한 달의 해당 일자부터 남은 일수 더하기
+        ans+=(numOfDays[m1]-d1+1);//+1한 이유는 자기 자신 포함해야 하기 때문
+        //cout<<numOfDays[m1]-d1<<"\n";
 
+        //끼이는 중간 달: 그냥 통으로 더해주기
+        for(int i=m1+1;i<m2;i++){
+            ans+=numOfDays[i];
+        }
+
+        //마지막달: d2 일수 더하기
+        ans+=d2; 
+    }
+    else{
+        ans+=(d2-d1+1);
+    }
+    
     cout<<ans<<"\n";
 
     return 0;
