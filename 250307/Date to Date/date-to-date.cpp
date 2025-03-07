a420924b39ee8aaeadb65318bb9d546d110a381a
HHS <43839695+hhes0225@users.noreply.github.com>
2025-03-07 17:15:40 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250307/Date to Date/date-to-date.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int m1, d1, m2, d2;
+
+int main() {
+    cin >> m1 >> d1 >> m2 >> d2;
+
+    int numOfDays[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+
+    int ans=0;
+
+    //시작 달: 한 달의 해당 일자부터 남은 일수 더하기
+    ans+=(numOfDays[m1]-d1+1);//+1한 이유는 자기 자신 포함해야 하기 때문
+    //cout<<numOfDays[m1]-d1<<"\n";
+
+    //끼이는 중간 달: 그냥 통으로 더해주기
+    for(int i=m1+1;i<m2;i++){
+        ans+=numOfDays[i];
+    }
+
+    //마지막달: d2 일수 더하기
+    ans+=d2;
+
+    cout<<ans<<"\n";
+
+    return 0;
+}
