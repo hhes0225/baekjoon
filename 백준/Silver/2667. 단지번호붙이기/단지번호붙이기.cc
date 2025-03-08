1214a1da97a1f15ebb3289c2ad78bcd65d432f81
HHS <43839695+hhes0225@users.noreply.github.com>
2025-03-09 05:10:36 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <queue>
 
 using namespace std;
 
@@ -12,8 +11,6 @@ using namespace std;
 	여기서 연결되었다는 것은 어떤 집이 좌우, 혹은 아래위로 다른 집이 있는 경우를 말한다. 
 	대각선상에 집이 있는 경우는 연결된 것이 아니다. <그림 2>는 <그림 1>을 단지별로 번호를 붙인 것이다. 
 	지도를 입력하여 단지수를 출력하고, 각 단지에 속하는 집의 수를 오름차순으로 정렬하여 출력하는 프로그램을 작성하시오.
-
- 	처음에는 DFS로만 풀었지만 연습삼아 BFS로도 풂
 */
 
 vector<vector<int>> map;
@@ -27,40 +24,6 @@ int moveY[4] = { 0, 0, -1, 1 };
 
 //단지 번호(main dfs 이후 초기화할 것)
 int complexNum = 1;
-
-void bfs(int x, int y) {
-	//첫 노드 방문 처리
-	visited[y][x] = true;
-
-	//큐 생성 및 첫 노드 push
-	queue<pair<int, int>> qsearch;
-	qsearch.push({ x, y });
-
-	while (!qsearch.empty()) {
-		//큐에서 dequeue(front pop)
-		pair<int, int> here = qsearch.front();
-		qsearch.pop();
-
-		for (int i = 0; i < 4; i++) {
-			int nextX = here.first + moveX[i];
-			int nextY = here.second + moveY[i];
-
-			//배열 인덱스 내부 범위인지 확인
-			if ((nextX >= 0 && nextX < mapSize) && (nextY >= 0 && nextY < mapSize)) {
-				//아파트 단지 있는지 확인
-				if (map[nextY][nextX] == 1) {
-					//방문 여부 확인-방문 안했으면 방문 처리 후 큐에 삽입+단지 개수 1씩 누적
-					if (visited[nextY][nextX] == false) {
-						visited[nextY][nextX] = true;
-						qsearch.push({ nextX, nextY });
-						complexNum++;
-					}
-				}
-			}
-		}
-
-	}
-}
 
 void dfs(int x, int y) {
 	//dfs는 우선 방문처리
@@ -120,8 +83,7 @@ int main(void) {
 	for (int i = 0; i < mapSize; i++) {
 		for (int j = 0; j < mapSize; j++) {
 			if (map[i][j] == 1 && visited[i][j] == false) {
-				bfs(j, i);// 배열에서 x와 y는 반대
-				//dfs(j, i);// 배열에서 x와 y는 반대
+				dfs(j, i);// 배열에서 x와 y는 반대
 				apartment.push_back(complexNum);
 				complexNum = 1;//단지 번호 초기화
 			}
