d2623ea399d40b37840c073a205e8c70c8277f74
HHS <43839695+hhes0225@users.noreply.github.com>
2025-03-18 05:21:07 +0900

    Title: 흰검 칠하기, Time: 9ms, Memory: 5MB, Status: Passed - CodeTree

--- 250318/흰검 칠하기/painting-white-black.cpp
+++ 250318/흰검 칠하기/painting-white-black.cpp
@@ -6,7 +6,9 @@ const int OFFSET = 200'000;
 int n;
 vector<pair<int, char>> command;
 vector<int> tiles;
-vector<int> visited;
+vector<int> bVisited;
+vector<int> wVisited;
+
 int black=0;
 int white=0;
 int gray=0;
@@ -16,7 +18,8 @@ int main() {
     command.resize(n);
 
     tiles.assign(MAX, 0);
-    visited.assign(MAX, 0); 
+    bVisited.assign(MAX, 0); 
+    wVisited.assign(MAX, 0);
 
 
     for (auto &c:command) {
@@ -31,32 +34,35 @@ int main() {
 
         if(dir=='R'){//검은색으로
             for(int i=0;i<amt;i++){
-                if(visited[cur]<4) tiles[cur]=1;
-                visited[cur]++;
+                tiles[cur]=1;
+                bVisited[cur]++;
                 
                 if(i!=amt-1) cur++;
             }
         }
         else{//흰색으로
             for(int i=0;i<amt;i++){
-                if(visited[cur]<4) tiles[cur]=-1;
-                visited[cur]++;
+                tiles[cur]=-1;
+                wVisited[cur]++;
 
                 if(i!=amt-1) cur--;
             }
         }
+
+        // for(int i=0;i<MAX;i++){
+        //     if(bVisited[i]==0&&wVisited[i]==0) continue;
+
+        //     cout<<i-OFFSET<<": "<<bVisited[i]<<", "<<wVisited[i]<<", "<<tiles[i]<<"\n";
+        // }
+        // cout<<"==================\n";
     }
 
-    // for(int i=0;i<MAX;i++){
-    //     if(visited[i]==0) continue;
-
-    //     cout<<i-OFFSET<<": "<<visited[i]<<", "<<tiles[i]<<"\n";
-    // }
+    
 
     for(int i=0;i<MAX;i++){
-        if(visited[i]==0) continue;
+        if(bVisited[i]==0&&wVisited[i]==0) continue;
 
-        if(visited[i]>=4){
+        if(bVisited[i]>=2 && wVisited[i]>=2){
             gray++;
             continue;
         }
