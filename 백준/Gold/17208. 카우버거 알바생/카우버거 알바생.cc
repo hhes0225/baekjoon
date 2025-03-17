e7b974e327f00e4a1dcb24b1d1a4a950db12b0b7
HHS <43839695+hhes0225@users.noreply.github.com>
2025-03-17 22:09:05 +0900

    [Gold IV] Title: 카우버거 알바생, Time: 44 ms, Memory: 39132 KB -BaekjoonHub

---
+++ 백준/Gold/17208. 카우버거 알바생/카우버거 알바생.cc
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    한정된 capacity 내의 최대 cost-> 냅색  
+
+    dp[i][burger][fries]
+    i번째 주문까지 처리했을 때,
+    버거 최대재고 burger와 감튀 최대재고 fries를 처리했을 때 
+    최대 주문 처리 수
+
+    현재 주문이 burger보다 적고 fries보다 적으면 주문 처리
+    그렇지 않으면 주문 처리 X
+
+    시간복잡도: O(n^3)..?
+    최대 주문 수=100 
+    최대 버거 수=300
+    최대 감튀 수=300
+    100*300*300 = 9'000'000
+    
+*/
+
+vector<pair<int, int>> orders;
+vector<vector<vector<int>>> dp;
+//dp[i][burger][fries]
+//i번째 주문까지 처리했을 때
+//버거 최대재고 burger와 감튀 최대재고 fries를 처리했을 때 
+//최대 주문 처리 수
+
+int getMaxOrder(int n, int m, int k){
+    for(int o=1;o<=n;o++){
+        auto [b, f]=orders[o];
+
+        for(int i=1;i<=m;i++){            
+            for(int j=1;j<=k;j++){
+                dp[o][i][j]=dp[o-1][i][j];
+                
+                if(i-b>=0 && j-f>=0){
+                    //cout<<o<<"번째 주문 처리!\n";
+                    dp[o][i][j]=max(dp[o][i][j], dp[o-1][i-b][j-f]+1);
+                }
+            }
+        }
+    }
+
+    return dp[n][m][k];
+}
+
+int main() {
+    int n, m, k;
+    cin>>n>>m>>k;
+
+    orders.resize(n+1);
+    dp.assign(n+1, vector<vector<int>>(m+1, vector<int>(k+1, 0)));
+
+    for(int i=1;i<=n;i++){
+        cin>>orders[i].first>>orders[i].second;
+    }
+
+    cout<<getMaxOrder(n, m, k)<<"\n";
+    
+    return 0;
+}
