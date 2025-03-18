8fa2325cdca72bcd55f411cb94c924442551a3dc
HHS <43839695+hhes0225@users.noreply.github.com>
2025-03-18 18:04:35 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250318/원소 10개의 합/sum-of-10-elements.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int sum=0;
+
+    for(int i=0;i<10;i++){
+        int tmp;
+        cin>>tmp;
+
+        sum+=tmp;
+    }
+
+    cout<<sum<<"\n";
+
+
+    return 0;
+}
