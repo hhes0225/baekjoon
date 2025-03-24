a78e9cc5b2c7e7a6f6126879437d45d5ed27b5f1
HHS <43839695+hhes0225@users.noreply.github.com>
2025-03-25 04:31:22 +0900

    Title: 특정 조건에 맞게 K개 중에 1개를 N번 뽑기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250325/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int K, N;
+unordered_map<int, int> dic;//연속 n번
+vector<int> choice;
+
+void backtracking(int n){
+    if(n==0){
+        for(auto c:choice){
+            cout<<c<<" ";
+        }
+        cout<<"\n";
+
+        return;
+    }
+
+    for(int i=1;i<=K;i++){
+        if(dic[i]<2){//같은거 또 추가하면 3번이 되니까, 같은 건 0,1번까진 괜춘
+            dic[i]++;//연속 누적 카운트
+            choice.push_back(i);
+
+            backtracking(n-1);
+            
+            //데이터 원복
+            dic[i]--;
+            choice.pop_back();
+        }
+        else{
+            dic[i]=0;//연속 카운트 초기화
+        }
+
+    }
+}
+
+int main() {
+    cin >> K >> N;
+
+    for(int i=1;i<=K;i++){
+        dic[i]=0;
+    }
+
+    backtracking(N);
+
+    
+
+    return 0;
+}
