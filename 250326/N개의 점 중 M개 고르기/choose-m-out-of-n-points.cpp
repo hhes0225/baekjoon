fa20ee9caf2fba0a6b2131313aa11aace8c201a9
HHS <43839695+hhes0225@users.noreply.github.com>
2025-03-26 19:16:01 +0900

    Title: N개의 점 중 M개 고르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250326/N개의 점 중 M개 고르기/choose-m-out-of-n-points.cpp
+++ 250326/N개의 점 중 M개 고르기/choose-m-out-of-n-points.cpp
@@ -5,16 +5,43 @@ int n, m;
 vector<pair<int, int>> dots;
 vector<pair<int, int>> choice;
 
+int minMaxDist=INT_MAX;
+
 //백트래킹+매개변수탐색..?
+//이 아니라 백트래킹+완전탐색
 
 int getDist(int x1, int y1, int x2, int y2){
     return (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);
 }
 
-void getMinDist()
+int groupMaxDist(){
+    int tmpDist=0;
+    for(int i=0;i<n;i++){
+        auto [x1, y1]=choice[i];
+        for(int j=i+1;j<n;j++){
+            auto [x2,y2]=choice[j];
+            tmpDist=max(tmpDist, getDist(x1,y1,x2,y2));
+        }
+
+        return tmpDist;
+    }
+}
+
+
 
 void backtracking(int idx, int choiceCnt){
-    if(choiceCnt==m){
+    if(idx==n){
+        if(choiceCnt==m){
+            // for(auto c:choice) cout<<c.first<<" "<<c.second<<"\n";
+            // cout<<"\n";
+            // cout<<groupMaxDist()<<"\n";
+            // cout<<"----------\n";
+
+            //최댓값의 최소화
+            minMaxDist=min(minMaxDist, groupMaxDist());
+            
+        }
+
         return;
     }
 
@@ -41,7 +68,10 @@ int main() {
     // Please write your code here.
     sort(dots.begin(), dots.end());
 
-    for(auto d:dots)cout<<d.first<<" "<<d.second<<"\n";
+    //for(auto d:dots)cout<<d.first<<" "<<d.second<<"\n";
+    backtracking(0, 0);
+
+    cout<<minMaxDist<<"\n";
 
     return 0;
 }
