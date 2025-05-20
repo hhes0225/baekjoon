f6a7ea92e3cb0232fe2ab19d361aaf0e7192b585
HHS <43839695+hhes0225@users.noreply.github.com>
2025-05-21 03:12:15 +0900

    [Gold V] Title: 감시 피하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.cc
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.cc
@@ -1,120 +1,83 @@
-#include <bits/stdc++.h>
+ #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<char>> grid;
+//https://transferhwang.tistory.com/565
+//dfs version
+
 int n;
+vector<vector<char>> grid;
+int dx[4]={-1,0,1,0};
+int dy[4]={0,-1,0,1};
+bool flag;
+vector<pair<int, int>> teacher, blank;//선생님 좌표, 빈칸 좌표
 
-void printGrid(){
-    for(auto i:grid){
-        for(auto j:i) cout<<j<<" ";
-        cout<<"\n";
+bool canWatch(int nx, int ny, int dir){
+    //한방향으로만 쭈우우욱
+    while(1){
+        nx+=dx[dir];
+        ny+=dy[dir];
+
+        if(nx<0||nx>=n||ny<0||ny>=n||grid[nx][ny]=='O') break;
+        //장애물 만나거나 범위 벗어나면 종료
+        //grid[nx][ny] 순서 맨 마지막으로 가야 함!!! 안그럼 outofbounds
+        if(grid[nx][ny]=='S') return true;//학생 만나면 감시 성공
     }
-    cout<<"\n";
+
+    return false;//감시 실패
 }
 
-bool meetStudent(int r, int c){
-    int m=0;
-    //선생님과 같은 row인가?
-    for(int i=r;i<n;i++){
-        if(grid[i][c]=='O') break;
-        if(grid[i][c]=='S') m++;
-    }
+bool isPossible(){
+    for(int i=0;i<teacher.size();i++){
+        auto [x,y]=teacher[i];
 
-    for(int i=r;i>=0;i--){
-        if(grid[i][c]=='O') break;
-        if(grid[i][c]=='S') m++;
-    }
-
-    //선생님과 같은 col인가?
-    for(int i=c;i<n;i++){
-        if(grid[r][i]=='O') break;
-        if(grid[r][i]=='S') m++;
-    }
-
-    for(int i=c;i>=0;i--){
-        if(grid[r][i]=='O') break;
-        if(grid[r][i]=='S') m++;
-    }
-
-    if(m==0) return true;
-    else return false;
-}
-
-bool canAvoidAll(){
-    bool notMeet=true;
-
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(grid[i][j]=='T'){
-                notMeet=(notMeet&&meetStudent(i, j));
-            }
+        for(int j=0;j<4;j++){
+            if(canWatch(x,y,j)) return false;//감시 성공->감시피하기 실패
         }
     }
-    // if(notMeet) cout<<"yes\n";
-    // else cout<<"no\n";
-    
-    return notMeet;
+
+    return true;//감시 전부 피했으므로 성공
 }
 
-
-int caset=0;
-bool avoidAllTeacher=false;
-
-void backtracking(int r, int c, int obLimit){
-    if(obLimit==0){
-        caset++;
-        avoidAllTeacher=(avoidAllTeacher||canAvoidAll());
-
-        // if(canAvoidAll()){
-        //     printGrid();
-        // }
+void dfs(int cnt){//cnt==장애물 설치 위치
+    if(cnt==3){
+        if(isPossible()) flag=true;
         return;
     }
 
-    if(avoidAllTeacher) return;
+    for(int i=0;i<blank.size();i++){
+        auto [x,y]=blank[i];
 
-    //row(서로 다른 row에 있어야 함)
-    for(int i=r+1;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(grid[i][j]=='X'){
-                grid[i][j]='O';
-                backtracking(i,j,obLimit-1);
-                grid[i][j]='X';
-            }
-        }
-    }
-
-    //col(서로 다른 col에 있어야 함)
-    for(int i=0;i<n;i++){
-        for(int j=c+1;j<n;j++){
-            if(grid[i][j]=='X'){
-                grid[i][j]='O';
-                backtracking(i,j,obLimit-1);
-                grid[i][j]='X';
-            }
+        if(grid[x][y]=='X'){
+            grid[x][y]='O';
+            dfs(cnt+1);
+            grid[x][y]='X';
         }
     }
 }
 
-int main() {
+int main(void){
     cin>>n;
 
     grid.resize(n, vector<char>(n));
 
     for(auto &i:grid){
-        for(auto &j:i) cin>>j;
+        for(auto&j:i){
+            cin>>j;
+        }
     }
 
-    
-    backtracking(-1,-1,3);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(grid[i][j]=='T') teacher.push_back({i, j});
+            else if(grid[i][j]=='X') blank.push_back({i, j});
+        }
+    }
 
-    if(avoidAllTeacher){
-        cout<<"YES";
-    }
-    else{
-        cout<<"NO";
-    }
-    
-    
+    dfs(0);
+
+    if(flag) cout<<"YES";
+    else cout<<"NO";
+
+
     return 0;
 }
