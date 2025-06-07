bd7dd2c05aeb4383ddfc2a389bac1f20d78e0db4
HHS <43839695+hhes0225@users.noreply.github.com>
2025-06-08 03:49:50 +0900

    Title: 수들의 합 최대화하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250608/수들의 합 최대화하기/max-sum-of-numbers.cpp
+++ 250608/수들의 합 최대화하기/max-sum-of-numbers.cpp
@@ -4,14 +4,10 @@ using namespace std;
 int n;
 vector<vector<int>> grid;
 vector<int> nums;
-vector<int> visitedRow;
 vector<int> visitedCol;
 int maxSum=0;
 
-int dr[5]={0, -1,1,0,0};
-int dc[5]={0,0,0,-1,1};
-
-void backtracking(int colored, int r, int c){
+void backtracking(int colored, int r){
     if(colored==0){
         int tmp=0;
 
@@ -24,51 +20,25 @@ void backtracking(int colored, int r, int c){
         return;
     }
 
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(visitedRow[i] || visitedCol[j]) continue;
+    for(int j=0;j<n;j++){
+        if(visitedCol[j]) continue;
 
-            // cout<<"컷안됐다\n";
+        visitedCol[j]=true;
+        nums.push_back(grid[r][j]); 
 
-            visitedRow[i]=true;
-            visitedCol[j]=true;
-            nums.push_back(grid[i][j]); 
-            // cout<<nums.back()<<"\n";
+        backtracking(colored-1, r+1);
 
-            backtracking(colored-1, i, j);
-
-            visitedRow[i]=false;
-            visitedCol[j]=false;
-            nums.pop_back();
-        }
+        visitedCol[j]=false;
+        nums.pop_back();
     }
-    // for(int i=0;i<5;i++){
-    //     int nextR=r+dr[i];
-    //     int nextC=c+dc[i];
-
-    //     if(nextR<0||nextR>=n||nextC<0||nextC>=n) continue;
-    //     if(visitedRow[nextR] || visitedCol[nextC]) continue;
-
-    //     // cout<<"컷안됐다\n";
-
-    //     visitedRow[nextR]=true;
-    //     visitedCol[nextC]=true;
-    //     nums.push_back(grid[nextR][nextC]); cout<<nums.back()<<"\n";
-
-    //     backtracking(colored-1, nextR, nextC);
-
-    //     visitedRow[nextR]=false;
-    //     visitedCol[nextC]=false;
-    //     nums.pop_back();
-    // }
 }
 
 int main() {
     cin >> n;
 
     grid.resize(n, vector<int>(n));
-    visitedRow.assign(n, false);
     visitedCol.assign(n, false);
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> grid[i][j];
@@ -76,7 +46,7 @@ int main() {
     }
 
     // Please write your code here.
-    backtracking(n, 0,0);
+    backtracking(n, 0);
     cout<<maxSum<<"\n";
 
     return 0;
