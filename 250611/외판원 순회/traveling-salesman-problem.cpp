45fbf8209261584dee0601be7d918e8b260802bd
HHS <43839695+hhes0225@users.noreply.github.com>
2025-06-11 13:49:19 +0900

    Title: 외판원 순회, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250611/외판원 순회/traveling-salesman-problem.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int A[10][10];
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> A[i][j];
+        }
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
