e647498176199c2cc142aac5250f0d66a8bcc8d6
HHS <43839695+hhes0225@users.noreply.github.com>
2025-06-22 04:25:47 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 544 ms, Memory: 209736 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cc
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cc
@@ -9,9 +9,13 @@ int main() {
 
     for(int i=1;i<=input.size();i++){
         for(int j=0;j<input.size();j++){
-            strSet.insert(input.substr(j,i));
+            string tmp=input.substr(j,i);
+
+            if(tmp.size()==i)
+                strSet.insert(tmp);
         }
     }
+
 
     cout<<strSet.size()<<"\n";
 
