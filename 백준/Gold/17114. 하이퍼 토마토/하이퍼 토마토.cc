31b16d1da8567553de4c5f6ff0ec905ba0a7cb38
HHS <43839695+hhes0225@users.noreply.github.com>
2025-06-24 18:55:49 +0900

    [Gold I] Title: 하이퍼 토마토, Time: 768 ms, Memory: 9720 KB -BaekjoonHub

--- 백준/Gold/17114. 하이퍼 토마토/하이퍼 토마토.cc
+++ 백준/Gold/17114. 하이퍼 토마토/하이퍼 토마토.cc
@@ -30,6 +30,7 @@ struct Pos{
     int m,n,o,p,q,r,s,t,u,v,w;
 };
 
+//넘나 괴로운 문제
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -39,10 +40,16 @@ int main() {
     int m,n,o,p,q,r,s,t,u,v,w;
     cin>>m>>n>>o>>p>>q>>r>>s>>t>>u>>v>>w;
 
+    /*
+        C++에서 다차원 배열은 오른쪽 인덱스가 가장 빠르게 변하며 
+        메모리에 연속 저장되기 때문에, 
+        연산 성능을 높이려면 가장 자주 순회하는 축을
+        배열의 마지막 인덱스로 두는 것이 좋다.
+    */
     int tomato[w][v][u][t][s][r][q][p][o][n][m];
     int vdays[w][v][u][t][s][r][q][p][o][n][m];
 
-    memset(vdays, -1, sizeof(vdays));
+    memset(vdays, -1, sizeof(vdays));//미방문==-1로
 
     int ans=0;
     int ripe=0;
@@ -57,11 +64,13 @@ int main() {
     for(int j=0;j<n;j++)for(int k=0;k<m;k++){
         cin>>tomato[a][b][c][d][e][f][g][h][i][j][k];
 
+        //그때그때 익은토마토면 0일로 방문처리하고 큐에 넣기
          if(tomato[a][b][c][d][e][f][g][h][i][j][k]==1){
              ripe++;
              vdays[a][b][c][d][e][f][g][h][i][j][k]=0;
-             que.push({{k,j,i,h,g,f,e,d,c,b,a},0});
+             que.push({{k,j,i,h,g,f,e,d,c,b,a},0});//큐에 넣을 때 순서 지키기
         }
+             //빈 공간이므로 총 토마토 개수에서 제외
         else if(tomato[a][b][c][d][e][f][g][h][i][j][k]==-1) totalTomato--;
     }
 
@@ -85,6 +94,7 @@ int main() {
             int nxtV=curPos.v+d[9];
             int nxtW=curPos.w+d[10];
 
+            //범위체크
             if(nxtM<0||nxtM>=m||
                 nxtN<0||nxtN>=n||
                 nxtO<0||nxtO>=o||
@@ -97,6 +107,7 @@ int main() {
                 nxtV<0||nxtV>=v||
                 nxtW<0||nxtW>=w) continue;
 
+                //안익은 토마토인데 미방문 상태라면(-1일) 
             if(tomato[nxtW][nxtV][nxtU][nxtT][nxtS][nxtR][nxtQ][nxtP][nxtO][nxtN][nxtM]==0
                && vdays[nxtW][nxtV][nxtU][nxtT][nxtS][nxtR][nxtQ][nxtP][nxtO][nxtN][nxtM]==-1){
                 vdays[nxtW][nxtV][nxtU][nxtT][nxtS][nxtR][nxtQ][nxtP][nxtO][nxtN][nxtM]=day+1;
